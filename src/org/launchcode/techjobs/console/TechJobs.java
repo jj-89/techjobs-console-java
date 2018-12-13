630fb59be2b9bfe8f9fc6f93a242435123989b6e
Jacob Johnson <jj.89@protonmail.com>
2018-12-13 14:08:58 -0600

    Implemented printJobs

--- src/org/launchcode/techjobs/console/TechJobs.java
+++ src/org/launchcode/techjobs/console/TechJobs.java
@@ -2,6 +2,7 @@ package org.launchcode.techjobs.console;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 /**
@@ -111,6 +112,20 @@ public class TechJobs {
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
 
-        System.out.println("printJobs is not implemented yet");
-    }
+        if(someJobs.size() == 0){
+            System.out.println("There are no jobs here.");
+            return;
+        }
+
+        for(HashMap<String, String> job : someJobs){
+            String jobInfo = "\n**********\n";
+
+            for(Map.Entry<String,String> jobColumn : job.entrySet()){
+                jobInfo += (jobColumn.getKey() + ":" + jobColumn.getValue() + "\n");
+            }
+
+            jobInfo += "**********";
+
+            System.out.println(jobInfo);
+        }    }
 }
